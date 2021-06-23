ee96637e3290b34fd4b5714ece1fbdf4f43cc5df
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-06-22 17:13:37 -0700

    Fix logger implementation

--- phoebus-alarm-logger/scripts/deploy-configuration.sh
+++ phoebus-alarm-logger/scripts/deploy-configuration.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v $2:/tmp/nalms/$1.xml -it phoebus start-logger $1 /tmp/nalms/$1.xml
+docker run -v $2:/tmp/nalms/$1.xml -it phoebus-alarm-logger start-logger $1 /tmp/nalms/$1.xml
