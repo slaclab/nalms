c1e2147ba2b096171a267fe03146bf28ca355d21
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-06-22 17:13:51 -0700

    Fix server implementation

--- phoebus-alarm-server/scripts/deploy-configuration.sh
+++ phoebus-alarm-server/scripts/deploy-configuration.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v $2:/tmp/nalms/$1.xml -it phoebus start-services $1 /tmp/nalms/$1.xml --softIoc 
+docker run -v $2:/tmp/nalms/$1.xml -it phoebus-alarm-server start-server $1 /tmp/nalms/$1.xml
