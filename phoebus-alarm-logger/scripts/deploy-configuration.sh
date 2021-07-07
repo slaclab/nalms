605cf7801a50fba9dad2a056450cc7952e6b5d3d
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-07-07 11:22:19 -0700

    Mount all files as volumes for alarm logger

--- phoebus-alarm-logger/scripts/deploy-configuration.sh
+++ phoebus-alarm-logger/scripts/deploy-configuration.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
-docker run -v $2:/tmp/nalms/$1.xml -it phoebus-alarm-logger start-logger $1 /tmp/nalms/$1.xml
+#
+# Author: Jacqueline Garrahan
+# Run phoebus-alarm-logger image for given configuration
+#
+
+
+if [ "$1" == "-h" ]; then
+  echo "Usage: deploy-configuration.sh [Configuration Name] [Configuration file] [Alarm logger properties file] [Logging configuration files]"
+  exit 0
+fi
+
+docker run -v $2:/tmp/nalms/$1.xml -v $3:/opt/nalms/config/alarm_logger.properties -v $4:/opt/nalms/config/logging.properties -t phoebus-alarm-logger start-logger $1 /tmp/nalms/$1.xml
