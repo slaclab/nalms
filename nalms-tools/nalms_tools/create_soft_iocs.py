b7505089682959ee2bc002567cf06f8581cf47db
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-07-16 09:53:21 -0700

    Update softIoc creation script

--- nalms-tools/nalms_tools/create_soft_iocs.py
+++ nalms-tools/nalms_tools/create_soft_iocs.py
@@ -1,62 +1,150 @@
 """
-Template file is generated using the base name of the configuration file. 
-By default, the template file used will be the default template packaged with nalms. 
+Python = 3.8
+Author: Jacqueline Garrahan
+
+Db and template files are generated using the base name of the configuration file. 
+By default, the template file used will be the default template packaged with NALMS. 
 This may instead be specified.
 
 """
 from lxml import etree
 import sys
 import os
+import string
 
-def create_soft_iocs(filename: str, template_file: str, output_directory: str, config_name: str = None) -> None:
+ALPHABET = string.ascii_uppercase
 
-    if not config_name:
-        config_name  = filename.split("/")[-1]
 
+def create_force_pvs(filename:str, output_directory:str, force_pv_template:str, config_name:str) -> str:
+    """Utility function for creating and writing forcepv template given a configuration.
+
+    Args:
+        filename (str): Name of configuration file
+        output_directory (str): Directory to write output db files
+        force_pv_template (str): Filename of forcepv template
+        config_name (str): Name of configuration
+
+    """
     # load file
     with open(filename, "r") as data:
         tree = etree.parse(data)
         root = tree.getroot()
 
-        # find automated_action entries
-        actions = root.findall(".//automated_action")
-        sevrpvs = []
+    # groups
+    groups = root.findall(".//component")
 
-        for action in actions:
-            details = action.findall(".//details")
-            for detail in details:
-                if "sevrpv" in detail.text:
-                    sevrpv = detail.text.strip("sevrpv:")
-                    sevrpvs.append(sevrpv)
+    # pvs
+    pvs = root.findall(".//pv")
 
-        if not template_file:
-            print("No template file provided.")
-            sys.exit()
+    forcepvs = [group.get("name") for group in groups] + [pv.get("name") for pv in pvs]
 
-        # create substitutions file
-        output_filename = f"{output_directory}/{config_name}.template"
-        with open(output_filename, "w") as f:
+    # create substitutions file
+    output_filename = f"{output_directory}/{config_name}.template"
+    with open(output_filename, "w") as f:
 
-            f.write(f"file {template_file} {{ ")
-            f.write("   pattern")
-            f.write("       {SEVRPVNAME}")
-            for sevrpv in sevrpvs:
-                f.write(f"       {{{sevrpv}}}")
-            f.write("}")
+        f.write(f"file {force_pv_template} {{")
+        f.write("   pattern")
+        f.write("       {PVNAME} \n")
+        for pv in forcepvs:
+            f.write(f"       {{{pv}}} \n")
+        f.write("}")
 
-        working_dir = os.getcwd()
+    return output_filename
 
-        # get working directory
-        with open(f"{output_directory}/st.cmd", "w") as f:
 
-            f.write(f"dbLoadTemplate(\"{output_filename}\") \n")
-            f.write("iocInit \n")
 
-        print(f"Created {output_filename} and st.cmd.")
+def create_summary_pvs(filename: strs, output_directory: str, config_name: str) -> str:
+    """Utility function for creating and writing summary db file.
+
+    Args:
+        filename (str): Name of configuration file
+        output_directory (str): Directory to write output db files
+        config_name (str): Name of configuration
+
+    """
+    # load file
+    with open(filename, "r") as data:
+        tree = etree.parse(data)
+        root = tree.getroot()
+
+    lines = []
+
+    # groups
+    groups = root.findall(".//component")
+    for group in groups:
+        name = group.get("name")
+        lines.append(f"record(calc, {name}:STATSUMY) {{\n")
+        lines+= ['field(DESC, "Summary PV")\n', 'field(SCAN, "1 second")\n']
+
+        child_idx = 0
+        for child in group.iterchildren():
+            child_name = child.get("name")
+            if child.tag == "component":
+                lines.append( f'field(INP{ALPHABET[child_idx]}, "{child_name}:STATSUMY.SEVR NPP MS")\n' )
+
+            elif child.tag == "pv":
+                lines.append( f'field(INP{ALPHABET[child_idx]}, "{child_name}:DP.SEVR NPP MS")\n' )
+
+            child_idx += 1
+
+        lines += ['field(CALC, "0")\n', '}\n\n']
+
+    # groups create FP summary
+    groups = root.findall(".//component")
+    for group in groups:
+        name = group.get("name")
+        lines.append(f"record(calc, {name}:STATSUMYFP) {{\n")
+        lines+= ['field(DESC, "Summary PV")\n', 'field(SCAN, "1 second")\n']
+
+        child_idx = 0
+        for child in group.iterchildren():
+            child_name = child.get("name")
+            if child.tag == "component":
+                lines.append( f'field(INP{ALPHABET[child_idx]}, "{child_name}:STATSUMYFP.SEVR NPP MS")\n' )
+
+            elif child.tag == "pv":
+                lines.append( f'field(INP{ALPHABET[child_idx]}, "{child_name}FP.SEVR NPP MS")\n' )
+
+            child_idx += 1
+
+        lines += ['field(CALC, "0")\n', '}\n\n']
+
+    # create substitutions file
+    output_filename = f"{output_directory}/NALMS_{config_name}.db"
+    with open(output_filename, "w") as f:
+        for line in lines:
+            f.write(line)
+
+    return output_filename
+
+
+def create_soft_ioc(filename: str, template_file: str, output_directory: str, config_name: str) -> None:
+    """Create softIoc from a configuration file.
+
+    Args:
+        filename (str): Name of configuration file
+        output_directory (str): Directory to write output db files
+        force_pv_template (str): Filename of forcepv template
+        config_name (str): Name of configurations
+
+    """
+
+    template_filename = create_force_pvs(filename, output_directory, template_file, config_name)
+    summary_pv_filename = create_summary_pvs(filename, output_directory, config_name)
+    
+    # get working directory
+    with open(f"{output_directory}/st.cmd", "w") as f:
+        f.write(f"dbLoadRecords(\"{summary_pv_filename}\") \n")
+        f.write(f"dbLoadTemplate(\"{template_filename}\") \n")
+        f.write("iocInit \n")
+
+    print(f"Created {template_filename}, {summary_pv_filename}, and st.cmd.")
+
+
 
 def main():
     if sys.argv[1] == "-h":
-        print("Create a substitutions file for configuration.")
+        print("Format softIOC for a NALMS configuration.")
         print(
             "Usage: python create_soft_iocs.py configuration_file template_file output_directory [config_name]"
         )
@@ -68,7 +156,11 @@ def main():
         )
 
     else:
-        create_soft_iocs(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        configuration_file = sys.argv[1]
+        template_file = sys.argv[2]
+        output_directory = sys.argv[3]
+        config_name = sys.argv[4]
+        create_soft_ioc(configuration_file, template_file, output_directory, config_name)
 
 if __name__ == "__main__":
     main()
