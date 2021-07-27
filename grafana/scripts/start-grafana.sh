903b31271ec616a4c5a9c44cbbbf64f2bb99b885
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-07-26 17:55:47 -0700

    Add comment

--- grafana/scripts/start-grafana.sh
+++ grafana/scripts/start-grafana.sh
@@ -3,6 +3,7 @@
 # create datasource from configurations
 bash /opt/nalms/create-datasource-file.sh 
 
+# create dashboards
 export IFS=","
 for CONFIG in $CONFIG_NAME; do
     sed 's/$DATASOURCE_NAME/'"$CONFIG/"  $DASHBOARD_TEMPLATE > "${DASHBOARD_DIR}/${CONFIG_NAME}.json"
