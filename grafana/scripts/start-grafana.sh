4b9837e7bf6698c1076c52d1d67dda9542d11381
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-07-29 11:17:08 -0700

    Drop update script from Grafana image

--- grafana/scripts/start-grafana.sh
+++ grafana/scripts/start-grafana.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-# create datasource from configurations
-bash /opt/nalms/create-datasource-file.sh 
 
 # create dashboards
 export IFS=","
-for CONFIG in $CONFIG_NAME; do
-    sed 's/$DATASOURCE_NAME/'"$CONFIG/"  $DASHBOARD_TEMPLATE > "${DASHBOARD_DIR}/${CONFIG_NAME}.json"
+for CONFIG_ITEM in $CONFIG_NAME; do
+    sed 's/$DATASOURCE_NAME/'"$CONFIG_ITEM/"  $DASHBOARD_TEMPLATE > "${DASHBOARD_DIR}/${CONFIG_ITEM}.json"
 done
 
 
+# create datasource from configurations
+bash /opt/nalms/create-datasource-file.sh 
+
 # update dashboards
-(bash /opt/nalms/update-dashboards.sh) &
+#(bash /opt/nalms/update-dashboards.sh) &
 
 #run
 /run.sh
